6e08d7756f954f7bdd9577b5b1f958a08d667393
D Gopall Krishna <nikhil0223@gmail.com>
2021-02-09 07:18:16 +0530

    deleted secondary(unnecessary) html file and changed names in views

--- certificate/urls.py
+++ certificate/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path,include, path
-from .views import Search, enter_id
+from .views import Search
 urlpatterns = [
     # re_path(r'^(?P<url_key>[0-9a-f-]+)/',Search,name='search'),
     path('<uuid:url_key>/', Search, name='search'),
-    path('', enter_id, name='enter_id'),
+    # path('', enter_id, name='enter_id'),
+    path('', Search, name='enter_id'),
+
 ]
 
--- certificate/views.py
+++ certificate/views.py
@@ -5,25 +5,26 @@ from django.shortcuts import get_object_or_404
 from .models import Certificate
 from .forms import CertificateForm
 
-def Search(request,url_key):
-    obj = get_object_or_404(Certificate,url_key=url_key)
 
-    if request.method == 'POST':
+def Search(request,url_key=None):
+    if url_key != None:
+
+        obj = get_object_or_404(Certificate,url_key=url_key)
+        
+        if request.method == 'POST':
+            # form = CertificateForm(request.POST)
+            url_key = request.POST.get('url_key')
+            return redirect('search', url_key=url_key)
+
+        return render(request, "certificate/certificate.html", {'certificate':obj})
+
+    elif request.method == 'POST':
         # form = CertificateForm(request.POST)
         url_key = request.POST.get('url_key')
         return redirect('search', url_key=url_key)
 
-    return render(request, "certificate/certificate2.html", {'certificate':obj})
-
-
-
-def enter_id(request):
-
-    if request.method == 'POST':
-        # form = CertificateForm(request.POST)
-        url_key = request.POST.get('url_key')
-        return redirect('search', url_key=url_key)
     else:
-        form = CertificateForm()
-    return render(request, "certificate/certificate.html")
+        return render(request, "certificate/certificate.html")
+
+
     
